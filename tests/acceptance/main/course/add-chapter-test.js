aa3011b9aff27ef4f02c10d31d03b12b019aa0cc
Marcelo J. Both <marcelo.both@gmail.com>
2017-04-09 00:04:30 -0300

    Created chapter form and improved interface.

---
+++ tests/acceptance/main/course/add-chapter-test.js
@@ -0,0 +1,22 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'course-builder-ui/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | add chapter');
+
+test('visiting /courses/chapters/new', function(assert) {
+  let course = server.create('course');
+
+  visit(`/courses/${course.id}/chapters/new`);
+
+  andThen(() => {
+    assert.equal(currentURL(), `/courses/${course.id}/chapters/new`);
+  });
+
+  fillIn('#chapter-name', 'Chapter one');
+
+  click('.btn-save-chapter');
+
+  andThen(() => {
+    assert.equal(find('.total-of-chapters').text(), 'Number of chapters: 1');
+  });
+});
