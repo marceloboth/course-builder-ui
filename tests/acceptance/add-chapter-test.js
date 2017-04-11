b668242ed3d1a7236f99665b311855a12b48e06e
Marcelo J. Both <marcelo.both@gmail.com>
2017-04-11 09:11:51 -0300

    Fixed new content acceptance test.

--- tests/acceptance/add-chapter-test.js
+++ tests/acceptance/add-chapter-test.js
@@ -13,15 +13,18 @@ test('visiting /courses/chapters/new', function(assert) {
   });
 
   fillIn('#chapter-title', Array(257).join('e'));
+  fillIn('#chapter-order', '');
   andThen(() => {
     assert.equal(find('.chapter-title-error').text().trim(), 'Title is too long (maximum is 255 characters)');
+    assert.equal(find('.chapter-order-error').text().trim(), 'Order must be a number');
   });
 
   fillIn('#chapter-title', 'Chapter one');
+  fillIn('#chapter-order', '1');
 
   click('.btn-save-chapter');
 
   andThen(() => {
-    assert.equal(find('.total-of-chapters').text(), 'Number of chapters: 1');
+    assert.equal(find('.chapter:first').text(), 'Chapter one');
   });
 });
