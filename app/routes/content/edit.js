af79b8632d0e81280edbc98b2d98f4888bb1edcb
Marcelo J. Both <marcelo.both@gmail.com>
2017-04-14 18:47:51 -0300

    Discovered the right way to charge ember power select.

--- app/routes/content/edit.js
+++ app/routes/content/edit.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model({ content_id }) {
+  model(params) {
     return Ember.RSVP.hash({
-      content: this.store.findRecord('content', content_id, {
+      content: this.store.findRecord('content', params.content_id, {
         chapter: this.modelFor('chapters/chapter')
-      })
+      }),
+      chapters: this.modelFor('course').get('chapters')
     });
   },
 
   setupController(controller, model) {
     controller.set('content', model.content);
+    controller.set('chapters', model.chapters);
   }
 });
