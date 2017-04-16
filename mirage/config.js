ff8d21633bd75d8d53754db907b1a06079963a91
Marcelo J. Both <marcelo.both@gmail.com>
2017-04-16 19:40:18 -0300

    Created mock for oauth token

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,21 @@
 export default function() {
+  this.post('/oauth/token', (db, request) => {
+    if (request.requestBody.indexOf("username=admin&password=admin") >= 0) {
+      return new Response(200, {}, {
+        "access_token": "fake_token",
+        "token_type": "bearer",
+        "expires_in": 7200,
+        "refresh_token": "fake_refresh_token",
+        "created_at": 1491306107
+      });
+    } else {
+      return new Response(401, {}, {
+        "error": "invalid_grant",
+        "error_description": "Invalid user or password"
+      });
+    }
+  });
+
   this.namespace = 'api/v1';
 
   this.resource('courses');
