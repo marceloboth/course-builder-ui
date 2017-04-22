20fe07b995a43b8d8d90194bd66c70fced04b776
Marcelo J. Both <marcelo.both@gmail.com>
2017-04-22 10:07:14 -0300

    Created acceptance spec for login and session management.

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,17 +1,86 @@
-import { test } from 'qunit';
+import {
+  test
+} from 'qunit';
+
 import moduleForAcceptance from 'course-builder-ui/tests/helpers/module-for-acceptance';
+import Ember from 'ember';
+
+import {
+  currentSession,
+  invalidateSession,
+  authenticateSession
+} from 'course-builder-ui/tests/helpers/ember-simple-auth';
 
 moduleForAcceptance('Acceptance | login');
 
-test('logging in the course builder', function(assert) {
+test('visiting /login', function(assert) {
   visit('/login');
-
-  fillIn('.username-field', 'username');
-  fillIn('.password-field', 'password');
-
-  click('.authenticate-button');
-
-  andThen(function() {
+  andThen(() => {
     assert.equal(currentURL(), '/login');
   });
 });
+
+test('When user is unauthenticated should see authentication form', function(assert) {
+  invalidateSession(this.application);
+  visit('/');
+
+  andThen(() => {
+    const loginFormPresent = find('#login-form').length > 0 ? true : false;
+    assert.equal(loginFormPresent, true);
+  });
+});
+
+test('User must be able to authenticate by form', function(assert) {
+  server.create('user', 1);
+
+  invalidateSession(this.application);
+  visit('/');
+
+  fillIn('#username', 'admin');
+  fillIn('#password', 'admin');
+  click('.btn-login');
+
+  andThen(() => {
+    const session = currentSession(this.application);
+    const isAuthenticated = Ember.get(session, 'isAuthenticated');
+
+    assert.equal(isAuthenticated, true);
+  });
+});
+
+test('User must be noticed when authentication fail', function(assert) {
+  server.create('user', 1);
+
+  invalidateSession(this.application);
+  visit('/');
+
+  fillIn('#username', 'admin');
+  fillIn('#password', 'wrongpass');
+  click('.btn-login');
+
+  andThen(() => {
+    const session = currentSession(this.application);
+    const isAuthenticated = Ember.get(session, 'isAuthenticated');
+
+    assert.equal(isAuthenticated, false);
+
+    const isShowingLoginFails = find('.login-err').length > 0 ? true : false;
+    assert.equal(isShowingLoginFails, true);
+  });
+});
+
+test('User must be able to ending the session', function(assert) {
+  server.create('user', 1);
+
+  authenticateSession(this.application);
+  visit('/');
+  click('.btn-logout');
+
+  andThen(() => {
+    const session = currentSession(this.application);
+    const isAuthenticated = Ember.get(session, 'isAuthenticated');
+
+    assert.equal(isAuthenticated,false);
+  });
+});
+
